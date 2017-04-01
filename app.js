a7e5fd8ddf72db6f4dcea8d9917e982238b33fe1
Florian Blt <Florianblot059@gmail.com>
2017-04-01 17:42:08 +0200

    Add Konami code

--- app.js
+++ app.js
@@ -190,4 +190,19 @@ $(".boutonLumiere").click(function(){
   }
 });
 
+(function(){
+    var kKeys = [];
+    function Kpress(e){
+        kKeys.push(e.keyCode);
+        if (kKeys.toString().indexOf("38,38,40,40,37,39,37,39,66,65") >= 0) {
+            jQuery(this).unbind('keydown', Kpress);
+            kExec();
+        }
+    }
+    jQuery(document).keydown(Kpress);
+})(jQuery);
+function kExec(){
+   document.location.href="https://fr.wikipedia.org/wiki/Code_Konami"
+}
+
 
