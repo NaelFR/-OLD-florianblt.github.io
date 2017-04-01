ef0a00f902cb43da84c403b111e69220b1a8f693
Unknown <Florianblot059@gmail.com>
2017-04-01 15:47:24 +0200

    Canvas Color

--- app.js
+++ app.js
@@ -34,11 +34,11 @@
 
 		//Text
 		var ctx = $color[0].getContext('2d'); 
-    	ctx.font = "13pt Arial, Helvetica, sans-serif;";
-    	ctx.strokeStyle = "rgb(0,0,0)";
-    	ctx.fillStyle = "rgb(177,178,184)";
-    	ctx.textAlign = "center"
-    	ctx.fillText(name, 50, 50);
+    ctx.font = "13pt Arial, Helvetica, sans-serif;";
+    ctx.strokeStyle = "rgb(0,0,0)";
+    ctx.fillStyle = "rgb(177,178,184)";
+    ctx.textAlign = "center"
+    ctx.fillText(name, 50, 50);
 
 
 	})
@@ -71,6 +71,60 @@ $(".boutonLumiere").click(function(){
     document.getElementById('imgLink').innerHTML = '<img src="images/linkedinLight.png" alt="Linkedin" style="width: 35px;">';
     document.getElementById('imgGit').innerHTML = '<img src="images/githubLight.png" alt="Linkedin" style="width: 35px;margin-left:15%;margin-right:15%">';
     document.getElementById('imgMail').innerHTML = '<img src="images/mailLight.png" alt="Linkedin" style="width: 35px;">';
+
+    // Test
+    $('input.round').each(function(){
+      var $input = $(this);
+      var $div = $input.parent();
+      var canvasCASuppr = document.getElementById('Circle');
+      if( canvasCASuppr){
+        canvasCASuppr.parentNode.removeChild( canvasCASuppr);
+      }
+      var canvasCoASuppr = document.getElementById('CanColor');
+      if( canvasCoASuppr){
+        canvasCoASuppr.parentNode.removeChild( canvasCoASuppr);
+      }
+    });
+    $('input.round').each(function(){
+      var $input = $(this);
+      var $div = $input.parent();
+      var name = $input.data('name');
+      var min = $input.data('min');
+      var max = $input.data('max');
+      var ratio = (($input.val() - min)/ (max - min));
+
+      var $circle = $('<canvas Id="Circle" width="100px" height="100px"/>');
+      var $color = $('<canvas Id="CanColor" width="100px" height="100px"/>');
+      $div.append($circle);
+      $div.append($color);
+      
+      //Cercle fond
+      var ctx = $circle[0].getContext('2d');
+      ctx.beginPath(); // Début du chemin de tracé
+      ctx.arc(50,50,42.5,0,2*Math.PI); //(x centre, y centre, rayon, départ, arrivée);
+      ctx.lineWidth= 10; // Largeur 
+      ctx.strokeStyle= "#b1b3b8";
+      ctx.shadowOffsetX= 1;
+      ctx.shadowBlur= 2.5;
+      ctx.shadowColor= "rgba(177,179,184,0.1)";
+      ctx.stroke(); // Fin du chemin de tracé
+
+      // Cercle couleur
+      var ctx = $color[0].getContext('2d');
+      ctx.beginPath();
+      ctx.arc(50,50,42.5,-1/2*(Math.PI), ratio*2*(Math.PI) - 1/2*(Math.PI)); 
+      ctx.lineWidth= 10; 
+      ctx.strokeStyle= "#2c3e50";
+      ctx.stroke(); 
+
+      //Text
+      var ctx = $color[0].getContext('2d'); 
+      ctx.font = "13pt Arial, Helvetica, sans-serif;";
+      ctx.strokeStyle = "rgb(0,0,0)";
+      ctx.fillStyle = "rgb(52,73,94)";
+      ctx.textAlign = "center"
+      ctx.fillText(name, 50, 50);
+    });
   } else {
     $(".switch").animate({
       left:"0px",
@@ -83,6 +137,56 @@ $(".boutonLumiere").click(function(){
     document.getElementById('imgLink').innerHTML = '<img src="images/linkedin.png" alt="Linkedin" style="width: 35px;">';
     document.getElementById('imgGit').innerHTML = '<img src="images/github.png" alt="Linkedin" style="width: 35px;margin-left:15%;margin-right:15%">';
     document.getElementById('imgMail').innerHTML = '<img src="images/mail.png" alt="Linkedin" style="width: 35px;">';
+    $('input.round').each(function(){
+      var canvasCASuppr = document.getElementById('Circle');
+      if( canvasCASuppr){
+        canvasCASuppr.parentNode.removeChild( canvasCASuppr);
+      }
+      var canvasCoASuppr = document.getElementById('CanColor');
+      if( canvasCoASuppr){
+        canvasCoASuppr.parentNode.removeChild( canvasCoASuppr);
+      }
+    });
+    $('input.round').each(function(){
+      var $input = $(this);
+      var $div = $input.parent();
+      var name = $input.data('name');
+      var min = $input.data('min');
+      var max = $input.data('max');
+      var ratio = (($input.val() - min)/ (max - min));
+
+      var $circle = $('<canvas Id="Circle" width="100px" height="100px"/>');
+      var $color = $('<canvas Id="CanColor" width="100px" height="100px"/>');
+      $div.append($circle);
+      $div.append($color);
+      
+      //Cercle fond
+      var ctx = $circle[0].getContext('2d');
+      ctx.beginPath(); // Début du chemin de tracé
+      ctx.arc(50,50,42.5,0,2*Math.PI); //(x centre, y centre, rayon, départ, arrivée);
+      ctx.lineWidth= 10; // Largeur 
+      ctx.strokeStyle= "#2c3e50";
+      ctx.shadowOffsetX= 1;
+      ctx.shadowBlur= 2.5;
+      ctx.shadowColor= "rgba(177,179,184,0.1)";
+      ctx.stroke(); // Fin du chemin de tracé
+
+      // Cercle couleur
+      var ctx = $color[0].getContext('2d');
+      ctx.beginPath();
+      ctx.arc(50,50,42.5,-1/2*(Math.PI), ratio*2*(Math.PI) - 1/2*(Math.PI)); 
+      ctx.lineWidth= 10; 
+      ctx.strokeStyle= "#b1b3b8";
+      ctx.stroke(); 
+
+      //Text
+      var ctx = $color[0].getContext('2d'); 
+      ctx.font = "13pt Arial, Helvetica, sans-serif;";
+      ctx.strokeStyle = "rgb(0,0,0)";
+      ctx.fillStyle = "rgb(177,178,184)";
+      ctx.textAlign = "center"
+      ctx.fillText(name, 50, 50);
+    });
   }
 });
 
